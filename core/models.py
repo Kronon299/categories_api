8509e8bd1eee675f77f3c94916a4cf0cd06eb430
Stas_Klimovich <stasklimovich299@gmail.com>
2020-11-28 18:47:39 +0300

    verbose name and __str__ were added to Category model

--- core/models.py
+++ core/models.py
@@ -4,3 +4,10 @@ from django.db import models
 class Category(models.Model):
     parentCategory = models.ForeignKey('self', blank=True, null=True, related_name='subcategories', on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
